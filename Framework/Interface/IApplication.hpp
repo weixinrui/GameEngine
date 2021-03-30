88aa557ed37d459f11392c80202e17dfecb29112
wei <xwei0012@student.monash.edu>
2021-03-31 00:19:57 +0800

    add the BaseApplication class which is used as the main entry point

--- Framework/Interface/IApplication.hpp
+++ Framework/Interface/IApplication.hpp
@@ -1,14 +1,15 @@
 #pragma once
 #include "Interface.hpp"
+#include "IRuntimeModule.hpp"
 
 namespace My
 {
-    Interface IRuntimeModule
+    Interface IApplication:Implements IRuntimeModule
     {
         public:
-        virtual ~IRuntimeModule(){};
         virtual int Initialize() = 0;
         virtual void Finalize() = 0;
         virtual void Tick() = 0;
+        virtual bool IsQuit() = 0;
     };
 }
