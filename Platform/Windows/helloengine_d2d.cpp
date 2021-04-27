e1ea12f35a4af83acf1412302381e5fb43c00dfb
XinruiWei <weixinruiyuanban@outlook.com>
2021-04-27 14:41:01 +0800

    using DirectX API

--- Platform/Windows/helloengine_d2d.cpp
+++ Platform/Windows/helloengine_d2d.cpp
@@ -2,6 +2,52 @@
 #include <windows.h>
 #include <d2d1.h>
 
+ID2D1Factory *pFactory = nullptr;
+ID2D1HwndRenderTarget *pRenderTarget = nullptr;
+ID2D1SolidColorBrush *pLightSlateGrayBrush = nullptr;
+ID2D1SolidColorBrush *pCornflowerBlueBrush = nullptr;
+
+template<class T>
+inline void SafeRelease(T **ppInterfacetoRelease)
+{
+    if(*ppInterfacetoRelease != nullptr)
+    {
+        (*ppInterfacetoRelease)->Release();
+        (*ppInterfacetoRelease) = nullptr;
+    }
+}
+
+HRESULT CreateGraphicsResources(HWND hWnd)
+{
+    HRESULT hr = S_OK;
+    if(pRenderTarget == nullptr)
+    {
+        RECT rc;
+        GetClientRect(hWnd, &rc);
+        D2D1_SIZE_U size = D2D1::SizeU(rc.right - rc.left, rc.bottom - rc.top);
+        hr = pFactory->CreateHwndRenderTarget(D2D1::RenderTargetProperties(), D2D1::HwndRenderTargetProperties(hWnd, size),
+            &pRenderTarget);
+        
+        if(SUCCEEDED(hr))
+        {
+            hr = pRenderTarget->CreateSolidColorBrush(D2D1::ColorF(D2D1::ColorF::LightSlateGray), &pLightSlateGrayBrush);
+        }
+        
+        if(SUCCEEDED(hr))
+        {
+            hr = pRenderTarget->CreateSolidColorBrush(D2D1::ColorF(D2D1::ColorF::CornflowerBlue), &pCornflowerBlueBrush);
+        }
+    }
+    return hr;
+}
+
+void DiscardGraphicsResources()
+{
+    SafeRelease(&pRenderTarget);
+    SafeRelease(&pLightSlateGrayBrush);
+    SafeRelease(&pCornflowerBlueBrush);
+}
+
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
